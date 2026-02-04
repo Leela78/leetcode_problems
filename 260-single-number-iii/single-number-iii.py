efcd57db4bdce57cf8f40c59e2dd081cca24934d
N.Leela Durga Bhavani <nnadipallileela@gmail.com>
2026-02-04 18:55:59 +0530

    Time: 4 ms (32.02%) | Memory: 21.2 MB (12.77%) - LeetSync

---
+++ 260-single-number-iii/single-number-iii.py
@@ -0,0 +1,18 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        xor=0
+        for num in nums:
+            xor ^= num
+        diff=xor &(-xor)
+        a=0
+        b=0
+        for num in nums:
+            if num&diff:
+                a^= num
+            else:
+                b ^=num
+        return a ,b        
+
+
+
+           
