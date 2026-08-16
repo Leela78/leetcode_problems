48238ad99a61ff984ff3dfe049beef8aaaaf0987
N.Leela Durga Bhavani <nnadipallileela@gmail.com>
2026-08-16 11:03:59 +0530

    Time: 52 ms (44.69%) | Memory: 36.6 MB (66.43%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.py
@@ -7,12 +7,10 @@ class Solution:
         longest = 0
 
         for num in num_set:
-            # Check if this is the start of a sequence
+          
             if num - 1 not in num_set:
                 current = num
                 count = 1
-
-                # Count forward
                 while current + 1 in num_set:
                     current += 1
                     count += 1
