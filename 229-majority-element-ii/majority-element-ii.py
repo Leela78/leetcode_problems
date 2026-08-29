fb1e0a792ce2b07319f5ad93add5a3c7ee3d99a2
N.Leela Durga Bhavani <nnadipallileela@gmail.com>
2026-08-29 14:36:33 +0530

    Time: 11 ms (34.68%) | Memory: 22.6 MB (55.98%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.py
+++ 229-majority-element-ii/majority-element-ii.py
@@ -1,15 +1,47 @@
 class Solution:
-    def majorityElement(self, nums: List[int]) -> List[int]:
-        n=len(nums)
-        dic={}
-        result=[]
-        count=n//3
-        for i in range(n):
-            if nums[i] not in dic:
-                dic[nums[i]]=1
+    def majorityElement(self, nums):
+        n = len(nums)
+
+        cnt1 = 0
+        cnt2 = 0
+
+        el1 = 0
+        el2 = 0
+        for num in nums:
+
+            if cnt1 == 0 and num != el2:
+                el1 = num
+                cnt1 = 1
+
+            elif cnt2 == 0 and num != el1:
+                el2 = num
+                cnt2 = 1
+
+            elif num == el1:
+                cnt1 += 1
+
+            elif num == el2:
+                cnt2 += 1
+
             else:
-                dic[nums[i]]=dic.get(nums[i],0)+1
-        for num in dic:
-            if dic[num]> count:
-                result.append(num)
+                cnt1 -= 1
+                cnt2 -= 1
+        cnt1 = 0
+        cnt2 = 0
+
+        for num in nums:
+            if num == el1:
+                cnt1 += 1
+
+            if num == el2:
+                cnt2 += 1
+
+        result = []
+
+        if cnt1 > n // 3:
+            result.append(el1)
+
+        if cnt2 > n // 3 and el1 != el2:
+            result.append(el2)
+
         return result
